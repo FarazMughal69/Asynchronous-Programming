e36fc25f915fb665377f703cbcf728d42fac0000
Faraz Baig <Faraz.pu2020@gmail.com>
2024-07-28 01:39:34 +0500

    Stream Practice

--- bin/dart_asynchronous.dart
+++ bin/dart_asynchronous.dart
@@ -1,26 +1,58 @@
 // ignore_for_file: unused_local_variable
 
+import 'dart:async';
+
 import 'questions_model.dart';
 
-void main(List<String> arguments) async {
-  List<Questions> qstnLst = List.empty();
-  try {
-    qstnLst = await fetchQuestions(qstnLst);
-    await printQuestions(qstnLst);
-  } on Exception catch (ex) {
-    print(ex);
+void main() async {
+  //Create a Dart program that demonstrates the use of streams to handle real-time data updates.
+
+  // Create Stream by Controller
+  StreamController<DataModel> controller = StreamController();
+  bool isTimerClosed = false;
+  DateTime time = updateData(controller, isTimerClosed);
+  controller.stream.listen((newValue) {
+    print('Received: ${newValue.value}');
+  });
+
+  //Subscribe Stream
+  Stream<DataModel> stream = dataStream();
+  stream.listen((event) {
+    print('Value Received from Stream ${event.value}');
+  });
+}
+
+Stream<DataModel> dataStream() async* {
+  bool isStreamClosed = false;
+  Timer(Duration(minutes: 1), () {
+    isStreamClosed = true;
+  });
+  while (!isStreamClosed) {
+    await Future.delayed(Duration(seconds: 1));
+    String newValue = DateTime.now().toString();
+    yield DataModel(newValue);
   }
 }
 
-Future<void> printQuestions(List<Questions> qstnLst) async {
-  for (var questions in qstnLst) {
-    int optn = 1;
-    print(questions.question);
-    for (var option in questions.options) {
-      print("$optn $option");
-      optn++;
-    }
-  }
+DateTime updateData(
+    StreamController<DataModel> controller, bool isTimerClosed) {
+  DateTime time = DateTime.now();
+  Timer periodicTimer;
+  periodicTimer = Timer.periodic(Duration(seconds: 1), (timer) {
+    String newValue = DateTime.now().toString();
+    DataModel value = DataModel(newValue);
+    controller.add(value);
+  });
+  Timer(Duration(minutes: 1), () {
+    periodicTimer.cancel();
+    controller.close(); // Close the stream controller when done
+  });
+  return time;
+}
+
+class DataModel {
+  String value;
+  DataModel(this.value);
 }
 
 Future<List<Questions>> fetchQuestions(List<Questions> qstnLst) async {
